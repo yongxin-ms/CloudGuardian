ebfd78db6d6fd5074eeb6c88eb6a92b030d28af5
Will <will@i007.cc>
2025-03-15 18:25:35 +0800

    update

---
+++ stop_service.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if v2ray is running
+if systemctl is-active --quiet v2ray; then
+    echo "$(date +%Y%m%d-%H:%M:%M) Stopping v2ray..."
+    systemctl stop v2ray
+    echo "$(date +%Y%m%d-%H:%M:%M) v2ray stopped."
+fi
