71b38489382e4a06dda6641753b825e8d1a12de9
Will <will@i007.cc>
2025-03-16 14:38:02 +0800

    update

--- start_service.sh
+++ start_service.sh
@@ -6,3 +6,12 @@ if ! systemctl is-active --quiet v2ray; then
     systemctl start v2ray
     echo "$(date +%Y%m%d-%H:%M:%M) v2ray started."
 fi
+
+# Check if nginx is running
+if ! systemctl is-active --quiet nginx; then
+    echo "$(date +%Y%m%d-%H:%M:%M) Starting nginx..."
+    systemctl start nginx
+    echo "$(date +%Y%m%d-%H:%M:%M) nginx started."
+fi
+
+
