731e7a6da847a2d4e573e618664ab5c82c775475
Will <yongxin@microsoft.com>
2026-06-12 07:54:55 +0000

    fix bugs

--- show_status.sh
+++ show_status.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Check if v2ray is running
-if ! systemctl is-active --quiet v2ray; then
+if systemctl is-active --quiet v2ray; then
     echo "$(date +%Y%m%d-%H:%M:%M) v2ray is running."
 else
     echo "$(date +%Y%m%d-%H:%M:%M) v2ray is not running."
 fi
 
 # Check if nginx is running
-if ! systemctl is-active --quiet nginx; then
+if systemctl is-active --quiet nginx; then
     echo "$(date +%Y%m%d-%H:%M:%M) nginx is running."
 else
     echo "$(date +%Y%m%d-%H:%M:%M) nginx is not running."
 fi
 
 # Check if x-ui is running
-if ! systemctl is-active --quiet x-ui; then
+if systemctl is-active --quiet x-ui; then
     echo "$(date +%Y%m%d-%H:%M:%M) x-ui is running."
 else
     echo "$(date +%Y%m%d-%H:%M:%M) x-ui is not running."
